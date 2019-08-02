c0f488f19fba4518afd309558a1994abdf1c2dbc
Kaushik Datta <kdatta@isi.edu>
2019-08-02 09:27:26 -0400

    Added checkArray()

--- build_run_thr_gcc.sh
+++ build_run_thr_gcc.sh
@@ -14,14 +14,14 @@ do
     for NCOLS in 8
     do
 	# compile and run naive code with a cold cache (cc)
-	${CC} ${CFLAGS} -DCLEAR_L3_CACHE -DPRINT_ARRAYS -DNTHREADS=${NTHREADS} -DNROWS=${NROWS} -DNCOLS=${NCOLS} -c -o main_thr.o main_thr.c
+	${CC} ${CFLAGS} -DPRINT_ARRAYS -DCHECK_ARRAY -DNTHREADS=${NTHREADS} -DNROWS=${NROWS} -DNCOLS=${NCOLS} -c -o main_thr.o main_thr.c
 	${CC} ${CFLAGS} -DPRINT_ARRAYS -DNTHREADS=${NTHREADS} -DNROWS=${NROWS} -DNCOLS=${NCOLS} -c -o transpose_thr.o transpose_thr.c
-	${CC} ${CFLAGS} ${THRFLAGS} -o run_${NROWS}x${NCOLS}_naive main_thr.o transpose_thr.o
+	${CC} ${CFLAGS} ${THRFLAGS} -o run_${NROWS}x${NCOLS}_${NTHREADS}t_naive main_thr.o transpose_thr.o
 
 	# run with one thread per core
-	numactl -C 0-"$((${NTHREADS}-1))" -l ./run_${NROWS}x${NCOLS}_naive > ./results_${NROWS}x${NCOLS}_naive.txt
+	numactl -C 0-"$((${NTHREADS}-1))" -l ./run_${NROWS}x${NCOLS}_${NTHREADS}t_naive > ./results_${NROWS}x${NCOLS}_${NTHREADS}t_naive.txt
 
 	# clean up
-	rm -rf run_${NROWS}x${NCOLS}_naive main_thr.o transpose_thr.o *~
+	rm -rf run_${NROWS}x${NCOLS}_${NTHREADS}t_naive main_thr.o transpose_thr.o *~
     done
 done
--- main_thr.c
+++ main_thr.c
@@ -37,6 +37,21 @@ void printArray(double * restrict C, int nrows, int ncols) {
 }
 #endif
 
+#if defined CHECK_ARRAY
+// returns 1 if D is not a matrix of C, 0 otherwise
+int checkArray(double * restrict C, double * restrict D, int nrows, int ncols) {
+  int i, j;
+  for (i = 0; i < nrows; i++) {
+    for (j = 0; j < ncols; j++) {
+      if (D[j*nrows + i] != C[i*ncols + j]) {
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+#endif
+
 int main(int argc, char **argv) {
 #if defined CLEAR_L3_CACHE
   double *C;
@@ -89,27 +104,19 @@ int main(int argc, char **argv) {
     }
   }
 
-  // try colThreadedTranspose_naive()  
-  for (t=0; t<NTHREADS; t++) {
-    rc = pthread_create(&threads[t], &attr, &colThreadedTranspose_naive, (void *)t);
-    if (rc) {
-      printf("ERROR; return code from pthread_create() is %d\n", rc);
-      exit(-1);
-    }
-  }
-  // free attribute and wait for the other threads
-  pthread_attr_destroy(&attr);
-  for(t=0; t<NTHREADS; t++) {
-    rc = pthread_join(threads[t], &status);
-    if (rc) {
-      printf("ERROR; return code from pthread_join() is %d\n", rc);
-      exit(-1);
-    }
-  }
-
 #if defined PRINT_ARRAYS
   printf("B array:\n");
   printArray(B, NCOLS, NROWS);
+#endif
+
+#if defined CHECK_ARRAY
+  rc = checkArray(A, B, NROWS, NCOLS);
+  if (rc) {
+    printf("TRANSPOSE FAILED\n");
+  }
+  else {
+    printf("TRANSPOSE SUCCEEDED\n");
+  }
 #endif
 
   free(A);
