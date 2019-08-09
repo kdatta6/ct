ec75c4905b903c2888c4c5df31f370878e1fe6df
Kaushik Datta <kdatta@isi.edu>
2019-08-09 14:50:11 -0400

    Combined the two threaded naive functions into one

--- transpose.c
+++ transpose.c
@@ -185,11 +185,14 @@ void intrin8x8Transpose(double* restrict A, double* restrict B) {
 #elif THREADS
 #if defined ROW_NAIVE
 void *rowThreadedTranspose_naive(void *thrArg) {
+#elif defined COL_NAIVE
+void *colThreadedTranspose_naive(void *thrArg) {
+#endif
   DTYPE * restrict A;
   DTYPE * restrict B;
   long tid;
   int num_large_chunks, small_chunk_size, large_chunk_size;
-  int i_min, i_max;
+  int loop_min, loop_max;
   int i, j;
 
   threadArg *my_thrArg = (threadArg *)thrArg;
@@ -198,69 +201,43 @@ void *rowThreadedTranspose_naive(void *thrArg) {
   tid = (long)(my_thrArg->t);
 
   // divide the rows as evenly as possible among the threads
+#if defined ROW_NAIVE
   num_large_chunks = NROWS % NTHREADS;
   small_chunk_size = NROWS / NTHREADS;
+#elif defined COL_NAIVE
+  num_large_chunks = NCOLS % NTHREADS;
+  small_chunk_size = NCOLS / NTHREADS;
+#endif
   large_chunk_size = small_chunk_size + 1;
   
   if (tid < num_large_chunks) {
-    i_min = tid * large_chunk_size;
-    i_max = i_min + large_chunk_size;
+    loop_min = tid * large_chunk_size;
+    loop_max = loop_min + large_chunk_size;
   }
   else {
-    i_min = num_large_chunks * large_chunk_size + (tid - num_large_chunks) * small_chunk_size;
-    i_max = i_min + small_chunk_size;    
+    loop_min = num_large_chunks * large_chunk_size + (tid - num_large_chunks) * small_chunk_size;
+    loop_max = loop_min + small_chunk_size;    
   }
 
 #if defined PRINT_ARRAYS
+#if defined ROW_NAIVE
   printf("In rowThreadedTranspose_naive(), tid = %ld, i_min = %d, i_max = %d\n", tid, i_min, i_max);
+#elif defined COL_NAIVE
+  printf("In colThreadedTranspose_naive(), tid = %ld, j_min = %d, j_max = %d\n", tid, j_min, j_max);
 #endif
-  for (i = i_min; i < i_max; i++) {
+#endif
+
+#if defined ROW_NAIVE
+  for (i = loop_min; i < loop_max; i++) {
     for (j = 0; j < NCOLS; j++) {
+#elif defined COL_NAIVE
+  for (i = 0; i < NROWS; i++) {
+    for (j = loop_min; j < loop_max; j++) {
+#endif
       B[j*NROWS + i] = A[i*NCOLS + j];
     }
   }  
   
   pthread_exit((void*) tid);
 }
-#elif defined COL_NAIVE
-void *colThreadedTranspose_naive(void *thrArg) {
-  DTYPE * restrict A;
-  DTYPE * restrict B;
-  long tid;
-  int num_large_chunks, small_chunk_size, large_chunk_size;
-  int j_min, j_max;
-  int i, j;
-
-  threadArg *my_thrArg = (threadArg *)thrArg;
-  A = (DTYPE *)(my_thrArg->A);
-  B = (DTYPE *)(my_thrArg->B);
-  tid = (long)(my_thrArg->t);
-
-  // divide the rows as evenly as possible among the threads
-  num_large_chunks = NCOLS % NTHREADS;
-  small_chunk_size = NCOLS / NTHREADS;
-  large_chunk_size = small_chunk_size + 1;
-  
-  if (tid < num_large_chunks) {
-    j_min = tid * large_chunk_size;
-    j_max = j_min + large_chunk_size;
-  }
-  else {
-    j_min = num_large_chunks * large_chunk_size + (tid - num_large_chunks) * small_chunk_size;
-    j_max = j_min + small_chunk_size;    
-  }
-
-#if defined PRINT_ARRAYS
-  printf("In colThreadedTranspose_naive(), tid = %ld, j_min = %d, j_max = %d\n", tid, j_min, j_max);
-#endif
-
-  for (i = 0; i < NROWS; i++) {
-    for (j = j_min; j < j_max; j++) {
-      B[j*NROWS + i] = A[i*NCOLS + j];
-    }
-  }
-  
-  pthread_exit((void*) tid);
-}
-#endif
 #endif
